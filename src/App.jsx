0b6f68b2df75239e8d750bf44e6f3577d02fb2c1
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2024-01-29 20:08:18 +0000

    restructured files and created all pages, added bootstrap header

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Header from './components/Header';
+import ProjectGallery from './components/ProjectGallery';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+     
+<Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<Projects />} />
+        <Route path="/projectgallery/" element={<ProjectGallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectGallery.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ProjectGallery() {
+  return <div>This is a project gallery component</div>;
+}
